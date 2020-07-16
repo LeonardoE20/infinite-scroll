17bcf5323b0e61a5cd7f0dddffc9325b73987430
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-16 00:11:48 -0700

    added a fetch request by creating a async function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 const count = 10;
 const apiKey = 'trPrT2fIZQsfNYSN4R92IsVc2w_adimyLV95f-SGLE4';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// On Load
+getPhotos();
