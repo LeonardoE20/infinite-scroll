e9d3116fa1a1aa56a7fbe6e0fe9ffbd1eb4adaf7
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-16 00:05:42 -0700

    added the api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Unsplash API
 const count = 10;
-const apiKey = '';
+const apiKey = 'trPrT2fIZQsfNYSN4R92IsVc2w_adimyLV95f-SGLE4';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
